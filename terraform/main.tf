8b6470ff6c1132a6fc38164e0cc74d4f22adcf94
mannvaswani4 <mannvaswani4@gmail.com>
2026-05-02 19:07:40 +0530

    Configure S3 remote backend for Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "commercio-assets-evolving-cow"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
